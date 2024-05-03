12737c79d59dca5e050fb734c5790544547389c0
FatAlien <vladislav.chigrinov@gmail.com>
2024-05-03 12:50:22 +0300

    Add complete achievement api query

--- AchieveClub.Server/Controllers/CompletedAchievementsController.cs
+++ AchieveClub.Server/Controllers/CompletedAchievementsController.cs
@@ -2,6 +2,7 @@
 using AchieveClubServer.Data.DTO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace AchieveClub.Server.Controllers
 {
@@ -22,7 +23,31 @@ namespace AchieveClub.Server.Controllers
             if (_db.Users.Count(x => x.Id == userId) == 0)
                 return BadRequest("User not found!");
 
-            return _db.CompletedAchievements.Where(ca=>ca.UserRefId == userId).Select(ca=>new CompletedAchievementState(ca.AchieveRefId)).ToList();
+            return _db.CompletedAchievements.Where(ca => ca.UserRefId == userId).Select(ca => new CompletedAchievementState(ca.AchieveRefId)).ToList();
         }
+
+        [Authorize(Roles = "Supervisor, Admin")]
+        [HttpPost]
+        public ActionResult CompleteAchievement(CompleteAchievementModel model)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.Id == model.UserId);
+            var achievement = _db.Achievements.FirstOrDefault(u => u.Id == model.AchievementId);
+
+            if (user == null || achievement == null)
+                return BadRequest("UserId/AchieveId is invalid");
+
+            if (_db.CompletedAchievements.Count(ca => ca.UserRefId == user.Id && ca.AchieveRefId == achievement.Id) != 0)
+                return BadRequest("Multiple achievements not supported");
+
+            var supervisorId = int.Parse(HttpContext.User.Identities.First().Name);
+
+            _db.CompletedAchievements.Add(new CompletedAchievementDbo { UserRefId = user.Id, AchieveRefId = achievement.Id, DateOfCompletion = DateTime.Now, SupervisorRefId = supervisorId });
+
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        public record CompleteAchievementModel([Required, Range(1, double.PositiveInfinity)] int UserId, [Required, Range(1, double.PositiveInfinity)] int AchievementId);
     }
 }
