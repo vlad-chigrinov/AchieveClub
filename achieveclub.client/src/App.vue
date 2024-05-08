cac62f18f24290c5768be4d7d6496b4617e03cd0
FatAlien <vladislav.chigrinov@gmail.com>
2024-05-08 10:50:43 +0300

    Add frontend to api sample

--- achieveclub.client/src/App.vue
+++ achieveclub.client/src/App.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>Hello World From Vue!</h1>
+    <div v-if="achievements">
+        <template v-for="(item, key) in achievements" :key="key">
+            <h3>{{ item["title"] }}</h3>
+            <p>{{ item["description"] }}</p>
+        </template>
+    </div>
+    <div v-else>
+        <h2>Loading...</h2>
+    </div>
 </template>
+
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const achievements = ref<Array<any>>()
+
+onMounted(async () => {
+    var f = await fetch("/api/achievements");
+    achievements.value = await f.json();
+    console.log(achievements);
+})
+</script>
--- achieveclub.client/vite.config.ts
+++ achieveclub.client/vite.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "^/api/ebat": {
+      "^/api/*": {
         target,
         secure: false,
       },
