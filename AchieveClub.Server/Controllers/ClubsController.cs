5681f388ca0f8160c4a19ec60b27bac150c7d4c4
FatAlien <vladislav.chigrinov@gmail.com>
2024-05-08 11:29:03 +0300

    Add get club titles endpoint

---
+++ AchieveClub.Server/Controllers/ClubsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AchieveClub.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubsController(ApplicationContext db) : ControllerBase
+    {
+        private ApplicationContext _db = db;
+
+        public record ClubName(int Id, string Title);
+
+        [HttpGet("titles")]
+        public ActionResult<List<ClubName>> GetClubTitles()
+        {
+            return _db.Clubs.Select(c => new ClubName(c.Id, c.Title)).ToList();
+        }
+    }
+}
